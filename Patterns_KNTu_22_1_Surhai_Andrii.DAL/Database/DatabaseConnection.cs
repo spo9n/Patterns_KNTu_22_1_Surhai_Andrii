7756d2f2052c6bdf3a4a38239add7b8d15096048
Andrii Surhai <andrewspoon888@gmail.com>
2023-11-23 16:19:33 +0200

    Видалено зайві коментарі.

--- Patterns_KNTu_22_1_Surhai_Andrii.DAL/Database/DatabaseConnection.cs
+++ Patterns_KNTu_22_1_Surhai_Andrii.DAL/Database/DatabaseConnection.cs
@@ -5,39 +5,16 @@ namespace Patterns_KNTu_22_1_Surhai_Andrii.DAL.Database
 {
     public class DatabaseConnection
     {
-        //private static MySqlConnection _connection;
-
-        //private static string _connectionString = "Server=localhost;Database=db_patterns;Uid=root;Pwd=123321;";
-
-
-        //private DatabaseConnection() { }
-
-        //public static MySqlConnection Connection
-        //{
-        //    get
-        //    {
-
-        //        if (_connection == null)
-        //        {
-        //            _connection = new MySqlConnection(_connectionString);
-        //        }
-        //        if (_connection.State != ConnectionState.Open)
-        //        {
-        //            _connection.Open();
-        //        }
-        //        return _connection;
-        //    }
-        //}
-
-
         private static DatabaseConnection _instance = null;
         private MySqlConnection _connection;
         private static readonly string _connectionString = "Server=localhost;Database=db_patterns;Uid=root;Pwd=123321;";
+
 
         private DatabaseConnection()
         {
             _connection = new MySqlConnection(_connectionString);
         }
+
 
         public static DatabaseConnection Instance
         {
@@ -51,6 +28,7 @@ namespace Patterns_KNTu_22_1_Surhai_Andrii.DAL.Database
                 return _instance;
             }
         }
+
 
         public MySqlConnection GetConnection()
         {
@@ -73,6 +51,7 @@ namespace Patterns_KNTu_22_1_Surhai_Andrii.DAL.Database
             return _connection;
         }
 
+
         public void CloseConnection()
         {
             if (_connection != null && _connection.State == ConnectionState.Open)
@@ -80,75 +59,5 @@ namespace Patterns_KNTu_22_1_Surhai_Andrii.DAL.Database
                 _connection.Close();
             }
         }
-
-
-
-        //public static MySqlConnection Connection
-        //{
-        //    get
-        //    {
-
-        //        if (_connection == null)
-        //        {
-        //            _connection = new MySqlConnection(_connectionString);
-        //        }
-        //        if (_connection.State != ConnectionState.Open)
-        //        {
-        //            _connection.Open();
-        //        }
-        //        return _connection;
-        //    }
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //public static MySqlConnection GetConnection()
-        //{
-        //    if (_connection == null)
-        //    {
-        //        _connection = new MySqlConnection(_connectionString);
-        //    }
-        //    if (_connection.State != ConnectionState.Open)
-        //    {
-        //        _connection.Open();
-        //    }
-        //    return _connection;
-        //}
-
-        //public void OpenConnection()
-        //{
-        //    if (_connection.State == ConnectionState.Closed)
-        //    {
-        //        _connection.Open();
-        //    }
-        //}
-
-        //public void CloseConnection()
-        //{
-        //    if (_connection.State == ConnectionState.Open)
-        //    {
-        //        _connection.Close();
-        //    }
-        //}
-
-        //public void Dispose()
-        //{
-        //    if (_connection != null)
-        //    {
-        //        _connection.Dispose();
-        //        _connection = null;
-        //    }
-        //}
-
     }
 }
